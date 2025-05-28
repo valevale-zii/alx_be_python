c1f781cab0c8bbf7d57b583d91a85acbd771a3d4
Valencia Masika <masikavalencia62@gmail.com>
2025-05-28 16:17:45 +0300

    Sum and addition

---
+++ python_introduction/sum_addition.py
@@ -0,0 +1,13 @@
+#Objective: To practice basic arithmetic operations in python
+
+#Step 1: Assign values to variables
+
+number1= 15
+number2= 10
+
+#Step 2: Perform arithmetic operations
+sum_result=number1 + number2
+
+#Step 3: Print the results
+
+print(f"The sum of {number1} and {number2} is {sum_result}.")
