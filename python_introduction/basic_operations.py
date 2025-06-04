b31a9a2147e03811f97d33ff271ece6a1ede0519
Valencia Masika <masikavalencia62@gmail.com>
2025-06-04 11:45:19 +0300

    Initial commit: Operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,10 @@
-# Assign values to the variables
-number1 = 10
-number2 = 5
+number1 = 15
+number2 = 11
 
-# Perform arithmetic operations
 sum_result = number1 + number2
 difference = number1 - number2
 product = number1 * number2
 
-# Print the results in a human-readable format
-print("Addition of", number1, "and", number2, "is", sum_result)
-print("Subtraction of", number1, "and", number2, "is", difference)
-print("Multiplication of", number1, "and", number2, "is", product)
+print("Addition:", number1, "+", number2, "=", sum_result)
+print("Subtraction:", number1, "-", number2, "=", difference)
+print("Multiplication:", number1, "*", number2, "=", product)
