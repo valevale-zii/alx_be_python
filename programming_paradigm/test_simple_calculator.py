8abac1b74e8c1a83941300108c9f648e87c0f6b5
Valencia Masika <masikavalencia62@gmail.com>
2025-06-22 14:21:17 +0300

    Initial commit: Python

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,34 @@
+# test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-5, -3), -8)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+        self.assertEqual(self.calc.subtract(2, 5), -3)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 5), -10)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(7, -1), -7)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero
+
+if __name__ == "__main__":
+    unittest.main()
